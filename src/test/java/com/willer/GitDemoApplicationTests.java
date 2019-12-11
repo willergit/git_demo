416679e26ac19daa1f1be1db33a8c4e6d957cf39
willergit <790995101@qq.com>
2019-12-11 09:56:09 +0800

    first commit

---
+++ src/test/java/com/willer/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
